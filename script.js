b69d147df2a23791f7107d6bafec674fc9c3b1ea
Prabhjot Singh <pjsingh1359@outlook.com>
2019-07-30 11:57:56 -0700

    cleaning up unused code and deleted console logs

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict';
 const diceTrigger = document.querySelector('.dice__trigger');
 let diceResult = document.querySelector('.dice__image');
-console.log(diceResult);
 const pewSound =  new Audio('pew.wav');
 
 function randomCount() {
@@ -16,38 +15,4 @@ const rollTheDice = () => {
   });
 };
 
-
-
-
 rollTheDice();
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// switch(count) {
-  // case 0: return diceResult;
-  // case 1: return diceResult = 'images/dice2.png';
-  // case 2: return diceResult = 'images/dice3.png';
-  // case 3: return diceResult = 'images/dice4.png';
-  // case 4: return diceResult = 'images/dice5.png';
-  // case 5: return diceResult = 'images/dice6.png';
-  // }
